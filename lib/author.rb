10149cdf4ef523b9984c05ee1d856457f8d0b0fa
Max Miller <maxmiller413@gmail.com>
2021-02-07 23:42:26 -0600

    commit tree db9b1500d86ab8d54ff439d26266d9d5532c7225

---
+++ lib/author.rb
@@ -0,0 +1,11 @@
+class Author
+    @@all = []
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+    def self.all
+        @@all
+    end 
+end 
---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+# class Song
+#     @@all = []
+#     attr_accessor :name, :artist
+#     def initialize(name)
+#         @name = name
+#         @@all << self
+#     end 
+#     def self.all
+#         @@all
+#     end 
+#     def artist_name
+#         artist.name if artist
+#     end
+# end 
+
+class Song
+    attr_accessor :name, :artist
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def artist_name
+      artist.name if artist
+    end
+  end
